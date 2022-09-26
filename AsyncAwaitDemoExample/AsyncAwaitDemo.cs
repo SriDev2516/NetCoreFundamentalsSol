4211db1a302dd76b1e483cfa308c6a99208741f5
Srinivas_Annavarpu <Srinivas_Annavarpu@W10F27WKG3>
2022-09-26 01:00:01 -0500

    Add project files.

---
+++ AsyncAwaitDemoExample/AsyncAwaitDemo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemoExample
+{
+    public class AsyncAwaitDemo
+    {
+        private const string URL = "https://docs.microsoft.com/en-us/dotnet/csharp/csharp";
+        public void DoSynchronousWork()
+        {
+            // You can do whatever work is needed here
+            Console.WriteLine("1. Doing some work synchronously");
+        }
+
+        public async Task DoSomeWorkAsync()
+        {
+            Console.WriteLine($"2.Async task has started...");
+            await GetStringLengthAsync(URL);
+        }
+
+        private async Task GetStringLengthAsync(string uRL)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                Console.WriteLine("3. Awaiting the result of GetStringAsync of Http Client...");
+                string result = await httpClient.GetStringAsync(URL);
+
+                Console.WriteLine("4. The awaited task has completed. Let's get the content length...");
+                Console.WriteLine($"5. The length of http Get for {URL}");
+                Console.WriteLine($"6. {result.Length} character");
+            }
+        }
+
+        public void DoSynchronousWorkAfterAwait()
+        {
+            //This is the work we can do while waiting for the awaited Async Task to complete
+            Console.WriteLine("7. While waiting for the async task to finish, we can do some unrelated work...");
+            for (var i = 0; i <= 5; i++)
+            {
+                for (var j = i; j <= 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ AsyncAwaitTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using AsyncAwaitDemoExample;
+using System;
+
+namespace AsyncAwaitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncAwaitDemo demo = new AsyncAwaitDemo();
+
+            demo.DoSynchronousWork();
+            var someTask = demo.DoSomeWorkAsync();
+            demo.DoSynchronousWorkAfterAwait();
+            someTask.Wait(); //this is a blocking call, use it only on Main method
+            Console.ReadLine();
+        }
+    }
+}
