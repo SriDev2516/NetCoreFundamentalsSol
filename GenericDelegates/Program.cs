39bcafcbbd6a26c6f004563336edebb6410be899
Srinivas_Annavarpu <Srinivas.Annavarpu@dellteam.com>
2022-10-14 16:05:46 -0500

    Lambda Expressions and Generic Delegates

---
+++ GenericDelegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GenericDelegates
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Func<int, double, float, double> delegate1 = (x, y, z) =>  (x + y + z); ;
+          
+            Action<int, double, float> delegate2 =  ( x,  y,  z) => Console.WriteLine(x + y + z);
+            
+            Predicate<string>isValidDelegate =  (str) =>
+            {
+                if (str.Length > 5)
+                {
+                    return true;
+                }
+
+                return false;
+            };
+
+            double result =  delegate1(2, 12, 12.3f);
+            Console.WriteLine(result);
+            delegate2(2, 12, 12.3f);
+            Console.WriteLine(isValidDelegate("Srinivas"));
+        }
+
+       
+    }
+}
---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        public delegate void GetDetailsDelegate(string name);
+        static void Main(string[] args)
+        {
+            GetDetailsDelegate getDetailsDelegate = (string name) =>
+            {
+                Console.WriteLine($"The name is {name}");
+            };
+            getDetailsDelegate.Invoke("Srinivas");
+        }
+    }
+}
